1630ee960908bd92b6c62222c5fab468d8736cf2
talkad <talkad@post.bgu.ac.il>
2022-06-11 19:55:04 +0300

    help me god

--- maane/src/main/java/Communication/Service/Interfaces/AnnualScheduleGeneratorService.java
+++
@@ -1,8 +0,0 @@
-package Communication.Service.Interfaces;
-
-import Domain.CommonClasses.Response;
-
-public interface AnnualScheduleGeneratorService {
-    public Response<Boolean> generateSchedule(String supervisor, String surveyId);
-
-}
--- maane/src/main/java/Communication/Service/Interfaces/MonthlyReportGenerator.java
+++
@@ -1,9 +0,0 @@
-package Communication.Service.Interfaces;
-
-import Domain.CommonClasses.Response;
-
-public interface MonthlyReportGenerator {
-
-    Response<byte[]> generateMonthlyReport(String username, int year, int month);
-
-}
